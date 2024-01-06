ef28ab2c3210afd28538c11cffd28962d332f6a3
illicchpv <illich58@mail.ru>
2024-01-06 16:41:15 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Main from './components/Main';
 const App = (props) => (<>
   <Header />
   <Main>
-    <h2>main.children</h2>
+    <h2>1main.children</h2>
     <h3>main.children</h3>
     <h4>main.children</h4>
     <h5>main.children</h5>
