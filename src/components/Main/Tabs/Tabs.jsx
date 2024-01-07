d20af11dd1788dddbd346c1a43b4febba3ecd0ea
illicchpv <illich58@mail.ru>
2024-01-07 15:36:02 +0300

    Урок 5 42:30

--- src/components/Main/Tabs/Tabs.jsx
+++ src/components/Main/Tabs/Tabs.jsx
@@ -8,11 +8,17 @@ import PropTypes from 'prop-types';
 import style from './Tabs.module.css';
 import { assignId } from '../../utils/genRandomId';
 
+import {ReactComponent as ArrowIcon} from './img/arrow.svg';
+import {ReactComponent as EyeIcon} from './img/eye.svg';
+import {ReactComponent as HomeIcon} from './img/home.svg';
+import {ReactComponent as PostIcon} from './img/post.svg';
+import {ReactComponent as SaveIcon} from './img/save.svg';
+
 const LIST = [
-  { value: 'Главная' },
-  { value: 'Просмотренные' },
-  { value: 'Сохранённые' },
-  { value: 'Мои посты' },
+  { value: 'Главная', Icon: EyeIcon },
+  { value: 'Просмотренные', Icon: HomeIcon },
+  { value: 'Сохранённые', Icon: PostIcon },
+  { value: 'Мои посты', Icon: SaveIcon },
 ].map(assignId);
 
 export const Tabs = (props) => {
@@ -22,13 +28,19 @@ export const Tabs = (props) => {
   return (
     <div className={style.container}>
       <div className={style.wrapperBtn}>
-        <button className={style.btn} onClick={() => setIsDropdownOpen(!isDropdownOpen)}>add item</button>
+        <button className={style.btn} onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+          add item
+          <ArrowIcon width={15} height={15} />
+        </button>
       </div>
 
       {isDropdownOpen && <ul className={style.list} onClick={() => setIsDropdownOpen(false)}>
         {LIST.map((el) => (
           <li className={style.item} key={el.id}>
-            <button className={style.btn} onClick={() => {console.log(el.value);}}>{el.value}</button>
+            <button className={style.btn} onClick={() => {console.log(el.value);}}>
+              {el.value}
+              {el.Icon && <el.Icon width={30} height={30} />}
+            </button>
           </li>
         ))}
       </ul>}
