861ee3306ff50eed6fb79ee471635baad32aa113
illicchpv <illich58@mail.ru>
2024-01-09 11:57:22 +0300

    Урок 7 tokenContext+

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import { tokenContext } from './context/tokenContext';
 import { useToken } from './hooks/useToken';
 
-// export const Layout = React.memo( (props) => {
-const App = (props) => {
+function App() {
   const [token, delToken] = useToken('');
+  const { Provider } = tokenContext;
 
-  return (<>
-    <Header token={token} delToken={delToken} />
-    <Main>
-      <h2>2main.children</h2>
-      <h3>3main.children</h3>
-      <h4>4main.children</h4>
-      <h5>5main.children</h5>
-    </Main>
-  </>);
+  return (
+    <Provider value={{ token, delToken }}>
+      <Header />
+      <Main />
+    </Provider>
+  );
 };
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,28 @@
-import PropTypes from 'prop-types';
+import React from 'react'
 import style from './Header.module.css';
 import Layout from '../Layout';
 import Logo from './Logo';
-import Heading from './Heading';
 import Search from './Search';
 import Auth from './Auth';
+import Heading from './Heading';
+import { tokenContext } from '../../context/tokenContext';
 
-export const Header = ({token, delToken}) => (
-  <header className={style.header}>
-    <Layout>
-      <div className={style.gridContainer}>
-        <Logo />
-        <Heading text='Главная' />
-        <Search />
-        <Auth token={token} delToken={delToken} />
-      </div>
-    </Layout>
-  </header>
-);
-Header.propTypes = {
-  token: PropTypes.string,
+export const Header = () => {
+  const {Consumer} = tokenContext;
+
+  return (
+    <header className={style.header}>
+      <Layout>
+        <div className={style.gridContainer}>
+          <Logo />
+          <Heading text='Главная' />
+          <Search />
+          <Consumer>
+            {(ctx) => <Auth token={ctx.token} delToken={ctx.delToken} />}
+          </Consumer>
+        </div>
+      </Layout>
+    </header>
+  )
 };
-export default Header;
+
