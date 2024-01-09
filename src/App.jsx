752118f1bce6080412ad8df2bf2573ea0f40242d
illicchpv <illich58@mail.ru>
2024-01-09 16:11:46 +0300

    Урок 7 Задача #1+

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import { AuthContextProvider } from './context/authContext';
-import { tokenContext } from './context/tokenContext';
+import { TokenContextProvider, tokenContext } from './context/tokenContext';
 import { useToken } from './hooks/useToken';
 
 function App() {
-  const [token, delToken] = useToken('');
 
   return (
-    <tokenContext.Provider value={{ token, delToken }}>
+    <TokenContextProvider>
       <AuthContextProvider>
         <Header />
         <Main />
       </AuthContextProvider>
-    </tokenContext.Provider>
+    </TokenContextProvider>
   );
 };
 export default App;
--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useAuth } from '../hooks/useAuth';
 
-
 export const authContext = React.createContext({});
 export const AuthContextProvider = ({children}) => {
   const [auth, clearAuth] = useAuth();
