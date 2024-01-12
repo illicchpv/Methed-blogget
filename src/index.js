af572fecd57a2efd977032e9d839738896a51527
illicchpv <illich58@mail.ru>
2024-01-12 16:59:28 +0300

    Урок 9 начало

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Урок 9
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
