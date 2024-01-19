85e92dc522e21cdfcedcc3a9082d5a15537ed720
illicchpv <illich58@mail.ru>
2024-01-19 09:53:17 +0300

    Урок 12 - заменили в store/index.js redux-devtools... -> reduxjs/toolkit. всё работает

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,24 @@
-import {combineReducers, createStore, applyMiddleware} from "redux";
-import {composeWithDevTools} from '@redux-devtools/extension';
+// import {combineReducers, createStore, applyMiddleware} from "redux";
+// import {composeWithDevTools} from '@redux-devtools/extension';
 import {commentReducer} from "./commentReducer";
 import {cutTabReducer} from "./cutTabReducer";
 import {tokenMidleware, tokenReducer} from "./tokenReducer";
-import {thunk} from 'redux-thunk';
+// import {thunk} from 'redux-thunk';
 import {authReducer} from './auth/authReducer';
 import {postsReducer} from './posts/postsReducer';
 import {postInfoReducer} from './postInfo/postInfoReducer';
 // import {xDataxReducer} from './xDatax/xDataxReducer'
+import {configureStore} from '@reduxjs/toolkit';
 
 // пример включения xDataxReducer   const rootReducer = combineReducers({tokenReducer, authReducer, postsReducer, postInfoReducer, xDataxReducer});
-const rootReducer = combineReducers({
-  cutTabReducer,
-  commentReducer,
-  tokenReducer,
-  authReducer,
-  postsReducer,
-  postInfoReducer
-});
+// const rootReducer = combineReducers({
+//   cutTabReducer,
+//   commentReducer,
+//   tokenReducer,
+//   authReducer,
+//   postsReducer,
+//   postInfoReducer
+// });
 
 // для примера напишем logger
 // const logger = (store) => (next) => (action) => {
@@ -26,8 +27,21 @@ const rootReducer = combineReducers({
 // };
 
 // если бы не было DevTools export const store = createStore(rootReducer, applyMiddleware());
-export const store = createStore(
-  rootReducer,
-  // composeWithDevTools(applyMiddleware(logger)); // для примера напишем логер
-  composeWithDevTools(applyMiddleware(tokenMidleware, thunk))
-);
+// export const store = createStore(
+//   rootReducer,
+//   // composeWithDevTools(applyMiddleware(logger)); // для примера напишем логер
+//   composeWithDevTools(applyMiddleware(tokenMidleware, thunk))
+// );
+
+export const store = configureStore({
+  reducer: {
+    cutTabReducer,
+    commentReducer,
+    tokenReducer,
+    authReducer,
+    postsReducer,
+    postInfoReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tokenMidleware),
+
+});
