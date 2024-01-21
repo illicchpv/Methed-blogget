145d51b7bbbfc95456683c108d25673a5cd7b41f
illicchpv <illich58@mail.ru>
2024-01-21 22:26:28 +0300

    Урок 12 - createAsyncThunk перетащил в postInfoAction

--- src/store/postInfo/postInfoAction.js
+++ src/store/postInfo/postInfoAction.js
@@ -1,62 +1,45 @@
-// import axios from "axios";
-// import {URL_API} from "../../api/const";
-// import {postInfoSlice} from './postInfoSlice';
-// import {createAsyncThunk} from "@reduxjs/toolkit";
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {URL_API} from "../../api/const";
+import axios from "axios";
 
-// export const POST_INFO_REQUEST = 'POST_INFO_REQUEST';
-// export const POST_INFO_REQUEST_SUCCESS = 'POST_INFO_REQUEST_SUCCESS';
-// export const POST_INFO_REQUEST_ERROR = 'POST_INFO_REQUEST_ERROR';
+export const postInfoRequestAsync = createAsyncThunk( // ??? Cannot access 'postsRequestAsync' before initialization
+  'postInfo/fetch',
+  (id, reduxTK) => {
+    const {getState, dispatch} = reduxTK;
 
-// export const postInfoRequest = () => ({
-//   type: POST_INFO_REQUEST,
-// });
-// export const postInfoRequestSuccess = (data) => ({
-//   type: POST_INFO_REQUEST_SUCCESS,
-//   data,
-// });
-// export const postInfoRequestError = (error) => ({
-//   type: POST_INFO_REQUEST_ERROR,
-//   error,
-// });
+    const token = getState().tokenReducer.token;
 
-/*
-export const postInfoRequestAsync = (id) => (dispatch, getState) => {
-  const token = getState().tokenReducer.token; // или useSelector(state => state.tokenReducer.token);
-  if (!token) return;
+    if (!token) return;
 
-  dispatch(postInfoRequest()); // ! это сбрасывает данные и выставляет loading = true
-
-  const url = `${URL_API}/comments/${id}?limit=30`;
-  console.log('postInfoRequestAsync: url', url);
-
-  axios(url, {// https://www.reddit.com/dev/api/#GET_api_v1_me
-    headers: {
-      Authorization: `bearer ${token}`, // https://github.com/reddit-archive/reddit/wiki/OAuth2#authorization-implicit-grant-flow
-    },
-  })
-    .then((data) => {
-      if (!data || !data.data) return;
-      debugger;
-      const post = data.data[0].data.children[0].data;
-
-      const children = data.data[1].data.children;
-      const c1 = children.map(item => item.data);
-      const comments = c1.filter((el) => {
-        if (!el.author) return false;
-        return true;
-      });
-
-      setTimeout(() => {
-        dispatch(postInfoRequestSuccess({post, comms: comments}));
-      }, 300);
+    // dispatch(postInfoRequest()); // ! это сбрасывает данные и выставляет loading = true
+  
+    const url = `${URL_API}/comments/${id}?limit=30`;
+    console.log('postInfoRequestAsync: url', url);
+  
+    return axios(url, {// https://www.reddit.com/dev/api/#GET_api_v1_me
+      headers: {
+        Authorization: `bearer ${token}`, // https://github.com/reddit-archive/reddit/wiki/OAuth2#authorization-implicit-grant-flow
+      },
     })
-    .catch((err) => {
-      dispatch(postInfoRequestError(err.message)); // ? err.toString()
-    });
-};
-*/
-/*
-
-
-*/
-
+      .then((data) => {
+        if (!data || !data.data) return;
+        const post = data.data[0].data.children[0].data;
+  
+        const children = data.data[1].data.children;
+        const c1 = children.map(item => item.data);
+        const comments = c1.filter((el) => {
+          if (!el.author) return false;
+          return true;
+        });
+  
+        return {post, comms: comments};
+        // setTimeout(() => {
+        //   dispatch(postInfoRequestSuccess({post, comms: comments}));
+        // }, 300);
+      })
+      .catch((err) => {
+        // dispatch(postInfoRequestError(err.message)); // ? err.toString()
+        return err.message;
+      });
+  }
+);
--- src/store/postInfo/postInfoSlice.js
+++ src/store/postInfo/postInfoSlice.js
@@ -1,50 +1,5 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import {MAX_AUTOLOAD, POSTS_COUNT, URL_API} from "../../api/const";
-import {uniqByKeepFirst} from "../../utils/uniqByKey";
-import axios from "axios";
-import {useSelector} from "react-redux";
-
-export const postInfoRequestAsync = createAsyncThunk( // ??? Cannot access 'postsRequestAsync' before initialization
-  'postInfo/fetch',
-  (id, reduxTK) => {
-    const {getState, dispatch} = reduxTK;
-
-    const token = getState().tokenReducer.token;
-
-    if (!token) return;
-
-    // dispatch(postInfoRequest()); // ! это сбрасывает данные и выставляет loading = true
-  
-    const url = `${URL_API}/comments/${id}?limit=30`;
-    console.log('postInfoRequestAsync: url', url);
-  
-    return axios(url, {// https://www.reddit.com/dev/api/#GET_api_v1_me
-      headers: {
-        Authorization: `bearer ${token}`, // https://github.com/reddit-archive/reddit/wiki/OAuth2#authorization-implicit-grant-flow
-      },
-    })
-      .then((data) => {
-        if (!data || !data.data) return;
-        const post = data.data[0].data.children[0].data;
-  
-        const children = data.data[1].data.children;
-        const c1 = children.map(item => item.data);
-        const comments = c1.filter((el) => {
-          if (!el.author) return false;
-          return true;
-        });
-  
-        return {post, comms: comments};
-        // setTimeout(() => {
-        //   dispatch(postInfoRequestSuccess({post, comms: comments}));
-        // }, 300);
-      })
-      .catch((err) => {
-        // dispatch(postInfoRequestError(err.message)); // ? err.toString()
-        return err.message;
-      });
-  }
-);
+import {createSlice} from "@reduxjs/toolkit";
+import {postInfoRequestAsync} from "./postInfoAction";
 
 const initialState = {
   post: {},
